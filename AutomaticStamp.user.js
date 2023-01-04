372f3ee79884f097195fe0559dd139d2c01944d1
9j4ck0 <9j4ck0@users.noreply.github.com>
2023-01-04 12:58:16 +0100

    Update AutomaticStamp.user.js

--- AutomaticStamp.user.js
+++ AutomaticStamp.user.js
@@ -3,7 +3,7 @@
 // @namespace    http//tampermonkey.net/
 // @version      0.0.2
 // @description  Automatic press Stamp for sheet open into tab
-// @author       9j3thr0 (giacomo.dallape)
+// @author       9j4ck0 (giacomo.dallape)
 // @match        https://statri-web.dipvvf.it/EditSheet/Edit/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
